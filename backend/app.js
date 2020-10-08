02b009e40ee11ef7516b325c8d66b805d35c6257
EdieAprovita <edieveg316@gmail.com>
2020-10-07 20:28:31 -0500

    Addin passport

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,14 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
+const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 const cors = require('cors');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
 
 mongoose
   .connect(process.env.DB, {
@@ -39,11 +43,48 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(logger('dev'));
 
+app.use(
+  require('node-sass-middleware')({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true,
+  })
+);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+hbs.registerHelper('ifUndefined', (value, options) => {
+  if (arguments.length < 2)
+    throw new Error('Handlebars Helper ifUndefined needs 1 parameter');
+  if (typeof value !== undefined) {
+    return options.inverse(this);
+  } else {
+    return options.fn(this);
+  }
+});
+
+app.use(
+  session({
+    secret: 'vegan',
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+app.use(flash());
+require('./passport')(app);
+
 const index = require('./routes/index');
 app.use('/', index);
 
 const todo = require('./routes/todo-routes');
 app.use('/api', todo);
+
+const auth = require("./routes/auth")
+app.use("/auth",auth)
 
 // Uncomment this line for production
 // app.get('/*', (req, res) => res.sendFile(__dirname + '/public/index.html'));
---
+++ backend/routes/auth.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const User = require('../models/User');
+
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
+
+router.get('/login', (req, res, next) => {
+  res.render('auth/login', { message: req.flash('error') });
+});
+
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/auth/login',
+    failureFlash: true,
+    passReqToCallback: true,
+  })
+);
+
+router.get('/signup', (req, res, next) => {
+  res.render('auth/signup');
+});
+
+router.post('/signup', (req, res, next) => {
+  const { email, password } = req.body;
+  if (email === '' || password === '') {
+    res.render('auth/signup', {
+      message: 'Please type your email and password',
+    });
+    return;
+  }
+
+  User.findOne({ email }, 'email', (error, email) => {
+    if (email !== null) {
+      res.render('auth/signup', { message: 'The email is already register' });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      email,
+      password: hashPass,
+    });
+
+    newUser
+      .save()
+      .then(() => {
+        res.redirect('/');
+      })
+      .catch((error) => {
+        res.render('auth/signup', { message: 'Something went wrong' });
+      });
+  });
+});
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
